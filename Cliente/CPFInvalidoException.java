1b2dacab6ecc0b4ff400020668b2d36de16810e4
binidj <38330384+binidj@users.noreply.github.com>
2018-06-18 09:14:18 -0300

    Delete CPFInvalidoException.java

--- Cliente/CPFInvalidoException.java
+++
@@ -1,9 +0,0 @@
-package Cliente;
- 
-public class CPFInvalidoException extends Exception{
-    private String CPF;
-    public CPFInvalidoException (String cpf) {
-        super ("O CPF" + cpf + "é invalido");
-        this.CPF = cpf;
-    }
-}
