c0aad3692ab0c30775ea9c5fbe4e4fae83e6dc15
binidj <38330384+binidj@users.noreply.github.com>
2018-06-18 09:15:12 -0300

    Delete NivelNaoDefinidoException.java

--- Cliente/NivelNaoDefinidoException.java
+++
@@ -1,9 +0,0 @@
-package Cliente;
- 
-public class NivelNaoDefinidoException extends Exception {
-    private String CPF;
-    public NivelNaoDefinidoException (String cpf) {
-        super ("Nivel não definido para o cliente" + cpf);
-        this.CPF = cpf;
-    }
-}
