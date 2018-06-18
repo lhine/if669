bed441cc8b24839d3a9c82a4a855b1ec33502d9f
binidj <38330384+binidj@users.noreply.github.com>
2018-06-18 09:33:17 -0300

    Add files via upload

--- Cliente/CadastroClientes.java
+++ Cliente/CadastroClientes.java
@@ -5,41 +5,31 @@ public class CadastroClientes {
 	public CadastroClientes (RepositorioCliente repositorio) {
 		this.clientes = repositorio;
 	}
-	public void cadastrar (Cliente cliente) throws ClienteJaCadastradoException, ClienteInvalidoException {
-		if (cliente.getCPF().length() == 11) {
-			if (!this.clientes.existe(cliente.getCPF())) {
-				this.clientes.inserir(cliente);
-			} else throw new ClienteJaCadastradoException (cliente);
-		} else throw new ClienteInvalidoException (cliente.getCPF());
+	public void cadastrar (Cliente cliente) throws ClienteJaCadastradoException {
+		if (!this.clientes.existe(cliente.getCPF())) {
+			this.clientes.inserir(cliente);
+		} else throw new ClienteJaCadastradoException (cliente);
 	}
-	public void remover (String cpf) throws ClienteNaoExisteException, ClienteInvalidoException {
-		if (cpf.length() == 11) {
+	public void remover (String cpf) throws ClienteNaoExisteException {
+		if (this.clientes.existe(cpf)) {
+			this.clientes.remover(cpf);
+		} else throw new ClienteNaoExisteException(cpf);
+	}
+	public void atualizar (Cliente cliente) throws ClienteNaoExisteException {
+		if (this.clientes.existe(cliente.getCPF())) {
+			this.clientes.atualizar(cliente);
+		} else throw new ClienteNaoExisteException(cliente.getCPF());
+	}
+	public Cliente procurar (String cpf) throws ClienteNaoExisteException {
+		if (this.clientes.existe(cpf)) {
+			return this.clientes.procurar(cpf);
+		} else throw new ClienteNaoExisteException (cpf);
+	}
+	public void mudarPlano (String cpf, Integer plano) throws PlanoInvalidoException, ClienteNaoExisteException {
+		if (plano > 0 && plano < 4) {
 			if (this.clientes.existe(cpf)) {
-				this.clientes.remover(cpf);
+				this.clientes.procurar(cpf).setPlano(plano);
 			} else throw new ClienteNaoExisteException(cpf);
-		} else throw new ClienteInvalidoException (cpf);
-	}
-	public void atualizar (Cliente cliente) throws ClienteNaoExisteException, ClienteInvalidoException {
-		if (cliente.getCPF().length() == 11) {
-			if (this.clientes.existe(cliente.getCPF())) {
-				this.clientes.atualizar(cliente);
-			} else throw new ClienteNaoExisteException(cliente.getCPF());
-		} else throw new ClienteInvalidoException (cliente.getCPF());
-	}
-	public Cliente procurar (String cpf) throws ClienteNaoExisteException, ClienteInvalidoException {
-		if (cpf.length() == 11) {
-			if (this.clientes.existe(cpf)) {
-				return this.clientes.procurar(cpf);
-			} else throw new ClienteNaoExisteException (cpf);
-		} else throw new ClienteInvalidoException (cpf);
-	}
-	public void mudarPlano (String cpf, Integer plano) throws PlanoInvalidoException, ClienteInvalidoException, ClienteNaoExisteException {
-		if (cpf.length() == 11) {
-			if (plano > 0 && plano < 4) {
-				if (this.clientes.existe(cpf)) {
-					this.clientes.procurar(cpf).setPlano(plano);
-				} else throw new ClienteInvalidoException(cpf);
-			} else throw new PlanoInvalidoException(plano);
-		} else throw new ClienteInvalidoException (cpf);
+		} else throw new PlanoInvalidoException(plano);
 	}
 }
--- Cliente/Cliente.java
+++ Cliente/Cliente.java
@@ -3,9 +3,11 @@ package Cliente;
 public class Cliente {
 	private String CPF;
 	private Integer Plano;
-	public Cliente (String cpf) {
-		this.CPF = cpf;
-		this.Plano = null;
+	public Cliente (String cpf) throws ClienteInvalidoException {
+		if (cpf.length() == 11) {
+			this.CPF = cpf;
+			this.Plano = null;
+		} else throw new ClienteInvalidoException (cpf);
 	}
 	public String getCPF() {
 		return this.CPF;
